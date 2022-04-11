ab83240b94947c6ccc3d7ac0ca8f26d94ecdc5f9
NishantSachan <nashsachan1998@gmail.com>
2022-04-11 13:26:49 +0530

    tab added

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, FormControl } from '@angular/forms';
 
+
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
